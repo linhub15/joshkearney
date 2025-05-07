c4142e509b3883c658faa4e6883fa46935027393
Hubert Lin <linhub15@users.noreply.github.com>
2025-05-07 14:53:24 -0600

    feat: add some icons

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import vento from "lume/plugins/vento.ts";
 import lume from "lume/mod.ts";
 import tailwind from "lume/plugins/tailwindcss.ts";
+import icons from "lume/plugins/icons.ts";
 
 const site = lume({ src: "./src" });
 
@@ -11,5 +12,7 @@ site.use(vento({
 
 site.use(tailwind());
 site.add("style.css");
+
+site.use(icons());
 
 export default site;
